20aa6a03989a6cd209ad8c01185f6799887ad868
victorbarbosa <victor.hugo.p.barbosa@hotmail.com>
2016-12-15 19:13:16 -0200

    added services

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ var ang = angular.module('app', []);
 ang.controller('control', function ($scope) {
     $scope.People = [
         { age: 29, name: "Victor Barbosa",country:"Brazil" },
+        { age: 29, name: "Victor Barbosa",country:"Brazil" },
         { age: 44, name: "Justin Trudeau" ,country:"Canada" },
         { age: 69, name: "Stephen King",country:"USA" }
     ]
@@ -11,31 +12,6 @@ ang.controller('control', function ($scope) {
         delete   $scope.value;
              
     }
-    $scope.colors = [
-     {name:'black',  },
-     {name:'black',  },
-     {name:'white',  },
-     {name:'white',  },
-     {name:'red',    },
-     {name:'red',    },
-     {name:'yellow', },
-     {name:'yellow', }
-];
-
- $scope.list = [
-        { 'name' : "ipad" },
-        { 'name' : "ipad" },
-        { 'name' : "ipad" },
-        { 'name' : "ipod" },
-        { 'name' : "iMac" },
-        { 'name' : "iMac" },
-        { 'name' : "iMac" },
-        { 'name' : "iPhone" },
-        { 'name' : "iWatch" },
-        { 'name' : "iWatch" },
-        { 'name' : "iWatch" },
-        { 'name' : "iPeed" }
-        ];
     $scope.Delete = function (index) {
         $scope.People.pop(index)
     }
