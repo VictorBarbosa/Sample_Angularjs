2a03d3ed06e649ef4046f0430921b21effa794cb
victorbarbosa <victor.hugo.p.barbosa@hotmail.com>
2016-12-18 18:15:56 -0200

    new  template

--- scripts/config/route-config.js
+++ scripts/config/route-config.js
@@ -1,8 +1,11 @@
-angular.module('app').config(function($routeProvider){
-   
-$routeProvider.when("/",{
-    templateUrl:'views/user/list.html'
-}).when("/new",{
-    templateUrl:'views/user/new.html'
-})
+angular.module('app').config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: 'views/user/list.html'
+    }).when("/list", {
+        templateUrl: 'views/user/list.html'
+    }).when("/new", {
+        templateUrl: 'views/user/new.html'
+    }).otherwise({
+        templateUrl: 'views/pageError.html'
+      });// if page not found
 })
